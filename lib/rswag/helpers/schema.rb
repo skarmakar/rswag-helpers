bb5f843a76cc18b0ee0c99f1bccdefad9a14a580
Santanu Karmakar <skarmakar.personal@gmail.com>
2022-04-21 12:29:25 +0530

    Update request_body method to handle nested hash automatically

--- lib/rswag/helpers/schema.rb
+++ lib/rswag/helpers/schema.rb
@@ -4,8 +4,22 @@ module Rswag
   module Helpers
     class Schema
       class << self
-        def request_body
-          @request_body ||= { data: extract_defaults(schema[:properties][:data][:properties]) }
+        def request_body(source_hash = nil)
+          source_hash ||= schema
+    
+          if source_hash.keys.include?(:properties)
+            properties = source_hash[:properties]
+      
+            if properties.keys.length == 1
+              # has nested key
+              nested_key = properties.keys.first
+              { nested_key => request_body(properties[nested_key]) }
+            else
+              extract_defaults(properties)
+            end
+          else
+            extract_defaults(source_hash)
+          end
         end
 
         def extract_defaults(hash)
